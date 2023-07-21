a9c98684f447eba8d1a5b0274f5b9d475a67b5ed
Andre M. de Souza <andresouza44@gmail.com>
2023-07-21 18:11:27 -0300

    Inserindo HATEOAS e criação de hipermídias.

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -6,6 +6,7 @@ import com.example.springboot.repositories.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,9 @@ import java.util.List;
 
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class ProductController {
@@ -31,7 +35,15 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts() {
-        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+        List <ProductModel> productsList = productRepository.findAll();
+        if (!productsList.isEmpty()){
+            for (ProductModel product : productsList) {
+                UUID id = product.getIdProduct();
+               product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+            }
+
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productsList);
     }
 
     @GetMapping("/products/{id}")
@@ -41,6 +53,7 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
 
         }
+        product0.get().add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withRel("Product List"));
         return ResponseEntity.status(HttpStatus.OK).body((product0.get()));
     }
 
